2a3002353e7ae168ae55a5fa28d29387f3c69694
Javier Escalada <kerrigan29a@gmail.com>
2025-06-20 16:28:44 +0200

    Remove problematic imports

--- microdoc/__init__.py
+++ microdoc/__init__.py
@@ -3,15 +3,6 @@
 
 from ._version import __version__
 
-from . import doc2md
-from . import md2html
-from . import py2doc
-from . import doctest_utils
-
 __all__ = [
     "__version__",
-    "doc2md",
-    "md2html",
-    "py2doc",
-    "doctest_utils",
 ]
